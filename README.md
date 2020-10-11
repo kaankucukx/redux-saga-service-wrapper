b85b07698b525d405f430beae4153e9fcf0b3422
kaankucukx <kaankck@gmail.com>
2020-10-11 08:29:44 +0300

    readme update

--- README.md
+++ README.md
@@ -38,6 +38,7 @@ function* postServiceSaga({ searchQuery }) {
   }
 }
 ```
-````javascript 
-
+## Handling Errors
+```javascript 
+Loading .. ;)
 ```
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ interface ServiceWrapperSagaParams {
 }
 
 
-export function* serviceWrapperSaga({ fn, args }: ServiceWrapperSagaParams): Generator {
+export function* serviceWrapperSaga<T1 extends Array<any>>(fn: (...args: any[]) => any, handleError?: Map<number, () => void>, ...args: T1): Generator {
   const CancelToken = axios.CancelToken;
   const source = CancelToken.source();
   try {
     return yield call(fn, ...args, { cancelToken: source.token });
   } catch (e) {
     const { response }: { response: AxiosResponse } = e;
-    console.log(response, 'response');
-
+    console.error(response, 'serviceWrapperSaga response');
+    handleError?.get(response.status)?.()
   } finally {
     if (yield cancelled()) {
       yield source.cancel();
